30fd5ea2d5865841ae37220a8d0723d7447574b7
DutraGames <gabrieldutraamarante@gmail.com>
2021-07-28 12:11:09 -0300

    first commit the project

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [peso, setPeso] = useState('')
+  const [altura, setAltura] = useState('')
+
+  function calcular(){
+    if(peso !== '' && altura !== ''){
+      const alt = altura / 100
+      const imc = peso / (alt * alt)
+
+      if (imc < 18.5){
+        alert("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
+      }
+      else if(imc >= 18.5 && imc < 24.9){
+        alert("Você está no Peso! Seu IMC: " + imc.toFixed(2))
+      }
+      else if (imc >= 25 && imc < 29.9){
+        alert("Você está acima do peso! Seu IMC: " + imc.toFixed(2))
+      }
+      else{
+        alert("Você está com algum grau de Obesidade! Seu IMC: " + imc.toFixed(2))
+      }
+
+    }else{
+      alert("Falta preencher os campos!!")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Calcule seu IMC</Text>
+      <TextInput 
+      style={styles.input}
+      value={peso}
+      keyboardType="numeric"
+      placeholder="Digite o seu Peso em Kg"
+      onChangeText={(peso) => setPeso(peso)}
+      />
+      <TextInput 
+      style={styles.input}
+      value={altura}
+      keyboardType="numeric"
+      placeholder="Digite a sua Altura em cm"
+      onChangeText={(altura) => setAltura(altura)}
+      />
+      <TouchableOpacity onPress={calcular} style={styles.btn}>
+        <Text style={styles.txtbtn}>Calcular</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +60,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titulo:{
+    color: '#000',
+    fontSize: 30,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  btn:{
+    marginTop: 10,
+    backgroundColor:'#00F',
+    width:200,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  txtbtn:{
+    color: '#FFF',
+    fontSize: 20,
+  },
+  input:{
+    width: 200,
+    borderRadius: 20,
+    padding: 10,
+    margin: 15,
+    color: '#fff',
+    backgroundColor: '#000',
+  }
 });
---
+++ README.md
@@ -0,0 +1,31 @@
+# Calculadora IMC
+
+A realização de um simples aplicativo em React Native, mas que a função Calcular no (App.js), pode ser utilizada para a realização em qualquer outro código que deseje usar o cálculo de IMC.
+
+# 👨‍💻Technology:
+
+ * Expo
+ * React
+ * React Native
+
+📷Images:
+
+(App Base)
+
+Interface inicial do App de Cálculo de IMC
+
+(em branco)
+
+Alerta ao deixar os campos em brancos
+
+(Resposta alternativa)
+
+Uma das possíveis respostas
+
+(GIF APP)
+
+Aplicativo em execução
+
+## Direitos:
+
+Direitos Públicos
